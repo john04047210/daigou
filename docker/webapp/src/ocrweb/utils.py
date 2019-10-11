26e091d5bc3028b9740f043fd1cff6ad6f2815bc
peng.qiao <15829265520@163.com>
2019-10-11 23:57:46 +0900

    add download xlsx file

--- docker/webapp/src/ocrweb/utils.py
+++ docker/webapp/src/ocrweb/utils.py
@@ -20,7 +20,7 @@ class Utils(object):
             cur_ws = meter_wb.active
             cur_ws.title = '物品核对'
             cur_ws.column_dimensions['C'].width = 40
-            cur_ws.column_dimensions['D'].width = 80
+            cur_ws.column_dimensions['D'].width = 60
             cur_ws.append(wx_title)
             cur_ws.append(ws_header)
             zip_filepath = os.path.join(base_path, 'tmp', zip_filename)
@@ -32,6 +32,7 @@ class Utils(object):
                     cur_ws.cell(row=row_idx+2, column=2, value=order.get('book_name'))
                     cur_ws.cell(row=row_idx+2, column=3, value=order.get('book_goods'))
                     cur_ws.cell(row=row_idx+2, column=4, value=order.get('address'))
+                    cur_ws.row_dimensions[row_idx+2].height = 40
                     row_idx = row_idx + 1
                 meter_wb.save(xls_filepath)
                 zf.write(xls_filepath, xls_filename, compress_type=zipfile.ZIP_DEFLATED)
